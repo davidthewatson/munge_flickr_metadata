9a476becf0f04ad9a9d5285ca3befb46763ab11c
David Watson <davidthewatson@gmail.com>
2011-08-27 15:40:15 -0400

    Removed extraneous prints and updated comments.

--- munge.py
+++ munge.py
@@ -1,12 +1,11 @@
-import flickrapi # see http://stuvel.eu/flickrapi/documentation/
-import yaml
+import flickrapi # easy_install flickrapi
+import yaml # easy_install pyyaml
 import sys
 
 def format_tags(tags):
     output = ''
     for tag in tags:
         txtstr = ''
-        #print tag.attrib
         tagstr =  tag.attrib['label']
         for z in tag: 
             if z.tag in ('clean'):
@@ -23,7 +22,6 @@ if not token: raw_input("Press ENTER after you authorize this program")
 flickr.get_token_part_two((token, frob))
 set_title = flickr.photosets_getInfo(photoset_id=sys.argv[1])[0].getchildren()[0].text
 this_set = flickr.walk_set(sys.argv[1])
-print dir(this_set)
 tag_set = ('Aperture', 'FocalLength', 'Focal Length', 'ISO', 'ISOSpeed', 'ISO Speed', 'Make', 'Model', 'Exposure', 'ExposureTime', 'Exposure Time') 
 for photo in this_set:
         photo_id = photo.get('id')
@@ -34,10 +32,6 @@ for photo in this_set:
                 continue
         msg = ''
         for x in exif:
-                tag_set = ('Aperture', 'FocalLength', 'Focal Length', 'ISO', 'ISOSpeed', 'ISO Speed', 'Make', 'Model', 'Exposure', 'ExposureTime', 'Exposure Time')
                 msg += format_tags(y for y in x if y.attrib['tag'] in tag_set or y.attrib['label'] in tag_set)
                 
         flickr.photos_setMeta(photo_id=photo_id, title=set_title, description=msg)
-        print msg
-                                                                                                                                                                                                 
-                                                                                                                                        
